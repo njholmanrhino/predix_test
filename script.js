21ac3fbdab951a1305fbbb034ffefe630e89484e
njholmanrhino <89196948+njholmanrhino@users.noreply.github.com>
2024-09-23 16:30:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    const tableBody = document.querySelector('tbody');
+    
+    data.forEach(item => {
+      const row = `<tr>
+                    <td>${item.Player}</td>
+                    <td>${item.Points}</td>
+                  </tr>`;
+      tableBody.innerHTML += row;
+    });
+  });
